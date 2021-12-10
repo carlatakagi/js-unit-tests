dadc19c172054bde7dffc6f74e6ac726d0635bf4
carlatakagi <carlatakagi@gmail.com>
2021-12-09 22:42:46 -0400

    requisito numbers ok e testando requisito productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,17 +28,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!'); // apagar quando resolver os requisitos
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
-    expect(productDetails).isArray([]);
+    expect (Array.isArray(productDetails));
     // Teste se o array retornado pela função contém dois itens dentro. 
     expect(productDetails.length).toEqual(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
-    
+    expect(typeof productDetails()).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    
     // Teste se os dois productIds terminam com 123.
+    fail('Teste vazio!'); // apagar quando resolver os requisitos
   });
 });
